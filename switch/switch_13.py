1e2f4b355cfa38e12d6c4b4cb558504a157da9ee
秋田海人 <kaito@akitakaininnoMac-Pro.local>
2017-08-01 05:18:57 +0900

    match add macadress

--- switch/switch_13.py
+++ switch/switch_13.py
@@ -19,6 +19,8 @@ from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
+from ryu.lib.packet import ipv4
+from ryu.lib.packet import vlan
 from ryu.lib.packet import ethernet
 
 
@@ -36,59 +38,53 @@ class ExampleSwitch13(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # install the table-miss flow entry.
-        # match = parser.OFPMatch()
-        # actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-        #                                   ofproto.OFPCML_NO_BUFFER)]
-        # self.add_flow(datapath, 0, match, actions)
-
         dpid = datapath.id
 
-        # s1 flow add
+        # flow add s1
         if dpid == 1:
             # host1 host3
-            actions = [parser.OFPActionOutput(11)]
-            match = parser.OFPMatch(eth_src="00:00:00:00:00:01",eth_dst="00:00:00:00:00:03")
+            actions = [parser.OFPActionOutput(1)]
+            match = parser.OFPMatch(in_port=4, eth_dst="00:00:00:00:00:01")
 
-            self.add_flow(datapath, 2, match, actions)
+            self.add_flow(datapath, 1, match, actions)
 
-            actions = [parser.OFPActionOutput(13)]
-            match = parser.OFPMatch(eth_src="00:00:00:00:00:03", eth_dst="00:00:00:00:00:01")
+            actions = [parser.OFPActionOutput(4)]
+            match = parser.OFPMatch(in_port=1, eth_dst="00:00:00:00:00:03")
 
-            self.add_flow(datapath, 2, match, actions)
+            self.add_flow(datapath, 1, match, actions)
 
             #host2 host4
-            actions = [parser.OFPActionOutput(11)]
-            match = parser.OFPMatch(in_port=12, eth_dst="00:00:00:00:00:04")
+            actions = [parser.OFPActionOutput(2)]
+            match = parser.OFPMatch(in_port=4, eth_dst="00:00:00:00:00:02")
 
             self.add_flow(datapath, 1, match, actions)
 
-            actions = [parser.OFPActionOutput(12)]
-            match = parser.OFPMatch(in_port=11, eth_dst="00:00:00:00:00:02")
+            actions = [parser.OFPActionOutput(4)]
+            match = parser.OFPMatch(in_port=2, eth_dst="00:00:00:00:00:04")
 
             self.add_flow(datapath, 1, match, actions)
 
-        # s2 flow add
+        # flow add s2
         if  dpid == 2:
             # host1 host3
-            actions = [parser.OFPActionOutput(10)]
-            match = parser.OFPMatch(eth_src="00:00:00:00:00:03", eth_dst="00:00:00:00:00:01")
-
-            self.add_flow(datapath, 2, match, actions)
-
-            actions = [parser.OFPActionOutput(12)]
-            match = parser.OFPMatch(eth_src="00:00:00:00:00:01", eth_dst="00:00:00:00:00:03")
-
-            self.add_flow(datapath, 2, match, actions)
-
-            #host2 host4
-            actions = [parser.OFPActionOutput(10)]
-            match = parser.OFPMatch(in_port=11, eth_dst="00:00:00:00:00:02")
+            actions = [parser.OFPActionOutput(1)]
+            match = parser.OFPMatch(in_port=4, eth_dst="00:00:00:00:00:03")
 
             self.add_flow(datapath, 1, match, actions)
 
-            actions = [parser.OFPActionOutput(11)]
-            match = parser.OFPMatch(in_port=10, eth_dst="00:00:00:00:00:04")
+            actions = [parser.OFPActionOutput(4)]
+            match = parser.OFPMatch(in_port=1, eth_dst="00:00:00:00:00:01")
+
+            self.add_flow(datapath, 1, match, actions)
+
+            #host2 host4
+            actions = [parser.OFPActionOutput(2)]
+            match = parser.OFPMatch(in_port=4, eth_dst="00:00:00:00:00:04")
+
+            self.add_flow(datapath, 1, match, actions)
+
+            actions = [parser.OFPActionOutput(4)]
+            match = parser.OFPMatch(in_port=2, eth_dst="00:00:00:00:00:02")
 
             self.add_flow(datapath, 1, match, actions)
 
@@ -103,51 +99,3 @@ class ExampleSwitch13(app_manager.RyuApp):
         mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
                                 match=match, instructions=inst)
         datapath.send_msg(mod)
-
-
-    # @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    # def _packet_in_handler(self, ev):
-    #     msg = ev.msg
-    #     datapath = msg.datapath
-    #     ofproto = datapath.ofproto
-    #     parser = datapath.ofproto_parser
-
-    #     # get Datapath ID to identify OpenFlow switches.
-    #     dpid = datapath.id
-    #     self.mac_to_port.setdefault(dpid, {})
-
-    #     # analyse the received packets using the packet library.
-    #     pkt = packet.Packet(msg.data)
-    #     eth_pkt = pkt.get_protocol(ethernet.ethernet)
-    #     dst = eth_pkt.dst
-    #     src = eth_pkt.src
-
-    #     # get the received port number from packet_in message.
-    #     in_port = msg.match['in_port']
-
-    #     self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
-
-    #     # learn a mac address to avoid FLOOD next time.
-    #     self.mac_to_port[dpid][src] = in_port
-
-    #     # if the destination mac address is already learned,
-    #     # decide which port to output the packet, otherwise FLOOD.
-    #     if dst in self.mac_to_port[dpid]:
-    #         out_port = self.mac_to_port[dpid][dst]
-    #     else:
-    #         out_port = ofproto.OFPP_FLOOD
-
-    #     # construct action list.
-    #     actions = [parser.OFPActionOutput(out_port)]
-
-    #     # install a flow to avoid packet_in next time.
-    #     if out_port != ofproto.OFPP_FLOOD:
-    #         match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
-    #         self.add_flow(datapath, 1, match, actions)
-
-    #     # construct packet_out message and send it.
-    #     out = parser.OFPPacketOut(datapath=datapath,
-    #                               buffer_id=ofproto.OFP_NO_BUFFER,
-    #                               in_port=in_port, actions=actions,
-    #                               data=msg.data)
-    #     datapath.send_msg(out)
