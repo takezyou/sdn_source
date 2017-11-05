306293f6285f8bc725f082eacb28f6ffd91193c8
秋田海人 <kaito@akitakaininnoMac-Pro.local>
2017-11-05 22:48:54 +0900

    modified lldp packet

--- switch/lldp_13.py
+++ switch/lldp_13.py
@@ -10,6 +10,8 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.ofproto import ether
 from ryu.ofproto import inet
 
+from ryu.lib import hub
+
 from ryu.lib.packet import packet
 from ryu.lib.packet import vlan
 from ryu.lib.packet import cfm
@@ -29,13 +31,14 @@ class Switch13(app_manager.RyuApp):
         super(Switch13, self).__init__(*args, **kwargs)
         # initialize mac address table.
         self.mac_to_port = {}
+        self.datapaths = []
         self.hw_addr = '88:d7:f6:7a:34:90'
         self.ip_addr = '10.50.0.100'
         self.vlan_type=ether.ETH_TYPE_8021Q
         self.ipv4_type=ether.ETH_TYPE_IP
         self.lldp_type=ether.ETH_TYPE_LLDP
-        self.cfm_type=ether.ETH_TYPE_ cfm
-        self.lldp_topo = {}
+        self.cfm_type=ether.ETH_TYPE_CFM
+        self.lldp_thread = hub.spawn(self.lldp_loop)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -43,12 +46,20 @@ class Switch13(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
+        self.datapaths.append(datapath)
+
         match = parser.OFPMatch()
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,ofproto.OFPCML_NO_BUFFER)]
 
         self.add_flow(datapath, 0, match, actions)
 
         self.send_port_desc_stats_request(datapath)
+    
+    def lldp_loop(self):
+        while True:
+            for dp in self.datapaths:
+                self.send_port_desc_stats_request(dp)
+            hub.sleep(10)
 
     def send_port_desc_stats_request(self, datapath):
         ofproto = datapath.ofproto
@@ -76,26 +87,26 @@ class Switch13(app_manager.RyuApp):
         
         for stat in ev.msg.body:
             if stat.port_no < ofproto.OFPP_MAX:
+                timestamp = time.time()
                 print "datapath:", datapath.id
                 print "prot:", stat.port_no
                 print "hard:", stat.hw_addr
-                self.send_lldp_packet(datapath, stat.port_no, stat.hw_addr)
+                self.send_lldp_packet(datapath, stat.port_no, stat.hw_addr, timestamp)
  
     
-    def send_lldp_packet (self, datapath, port_no, hw_addr):
+    def send_lldp_packet (self, datapath, port_no, hw_addr, timestamp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
         pkt = packet.Packet()
         pkt.add_protocol(ethernet.ethernet(ethertype=self.lldp_type, src=hw_addr, dst=lldp.LLDP_MAC_NEAREST_BRIDGE))
-
-        pkt.add_protocol(ethernet.ethernet(ethertype=self.lldp_type, src=hw_addr, dst=lldp.LLDP_MAC_NEAREST_BRIDGE))
  
         tlv_chassis_id = lldp.ChassisID(subtype=lldp.ChassisID.SUB_LOCALLY_ASSIGNED, chassis_id=str(datapath.id))
         tlv_port_id = lldp.PortID(subtype=lldp.PortID.SUB_LOCALLY_ASSIGNED, port_id=str(port_no))
         tlv_ttl = lldp.TTL(ttl=10)
+        tlv_time = lldp.TimeStamp(timestamp=timestamp)
         tlv_end = lldp.End()
-        tlvs = (tlv_chassis_id, tlv_port_id, tlv_ttl, tlv_end)
+        tlvs = (tlv_chassis_id, tlv_port_id, tlv_ttl, tlv_time, tlv_end)
         pkt.add_protocol(lldp.lldp(tlvs))
         pkt.serialize()
         data = pkt.data
@@ -125,10 +136,5 @@ class Switch13(app_manager.RyuApp):
             self.handle_lldp(datapath, port, pkt_lldp)
     
     def handle_lldp(self, datapath, port, pkt_lldp):
-        print "datapath:", datapath.id, "port:", port, "datapath:", pkt_lldp.tlvs[0].chassis_id, "port:", pkt_lldp.tlvs[1].port_id
-        
- 
-    @set_ev_cls(ofp_event.EventOFPErrorMsg,MAIN_DISPATCHER)
-    def error_msg_handler(self, ev):
-        msg = ev.msg
-        self.logger.debug('OFPErrorMsg received: type=0x%02x code=0x%02x message=%s',msg.type, msg.code, utils.hex_array(msg.data))
+        timestamp_diff = time.time() - pkt_lldp.tlvs[3].timestamp
+        print "datapath:", datapath.id, "port:", port, "datapath:", pkt_lldp.tlvs[0].chassis_id, "port:", pkt_lldp.tlvs[1].port_id, "delay", timestamp_diff 
